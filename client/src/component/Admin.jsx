b560209059ea1780dc1939956048fab2ff8123cb
anurag-17 <anurag.pandey17@gmail.com>
2022-11-16 19:34:15 +0530

    main

--- client/src/component/Admin.jsx
+++ client/src/component/Admin.jsx
@@ -2,7 +2,9 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import axios from 'axios';
 import { useNavigate } from 'react-router';
+import avatar from "./Images/logo_crop.png"
 import "./Admin.css"
+
 export const Admin = () => {
 
 const [token, setToken] = useState(JSON.parse(localStorage.getItem("newstoken")))
@@ -40,24 +42,27 @@ useEffect(()=>{
     }
     return (
         <>
-            <div style={{width:"50%"}} className="container mt-5">
-                <h3>Admin</h3>
-                <form className="was-validated" onSubmit={form_handler}>
-                    <div className="mb-3 mt-3">
-                        <label for="uname" className="form-label">Username:</label>
-                        <input type="email" className="form-control aa" id="uname" placeholder="Enter username" name="email" onChange={input_handler} required />
-                        <div className="valid-feedback">Valid.</div>
-                        <div className="invalid-feedback">Please fill out this field.</div>
+ <div className="contentAdmin">
+                <div className="login">
+                    <div className="avatar">
+                        <img src={avatar} />
                     </div>
-                    <div className="mb-3">
-                        <label for="pwd" className="form-label">Password:</label>
-                        <input type="password" className="form-control aa" id="pwd" placeholder="Enter password" name="password" onChange={input_handler} required />
-                        <div className="valid-feedback">Valid.</div>
-                        <div className="invalid-feedback">Please fill out this field.</div>
-                    </div>
+                    <h2>Login</h2>
+                    <h3>Welcome back Admin</h3>
 
-                    <input type="submit" value="Login" className='btn btn-primary' />
-                </form>
+                    <form className="login-form">
+                        <div className="textbox">
+                            <input type="email" placeholder="Username" id="uname" name="email" onChange={input_handler} required />
+                            <span class="material-symbols-outlined"> <i class="fa fa-user-circle-o" aria-hidden="true"></i> </span>
+                        </div>
+                        <div class="textbox">
+                            <input type="password" placeholder="Password" id="pwd" name="password" onChange={input_handler} required />
+                            <span class="material-symbols-outlined"><i class="fa fa-lock" aria-hidden="true"></i></span>
+                        </div>
+                        <button type="submit" onClick={form_handler}>LOGIN</button>
+                        {/* <a href="">Forgot your credentials?</a> */}
+                    </form>
+                </div>
             </div>
         </>
     )
