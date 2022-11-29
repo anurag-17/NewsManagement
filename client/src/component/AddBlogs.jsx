8b318388901bdba006501a7455aab2fc2464301c
Anurag Pandey <anurag.pandey17@gmail.com>
2022-11-29 12:43:21 +0530

    ck editor

--- client/src/component/AddBlogs.jsx
+++ client/src/component/AddBlogs.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useRef } from 'react'
-import { Editor } from '@tinymce/tinymce-react';
 import axios from 'axios';
 import { AdminDash } from './AdminDash';
 import Resizer from "react-image-file-resizer";
 import "./AddBlogs.css"
-
+import Editor from 'ckeditor5-custom-build/build/ckeditor';
+import { CKEditor } from '@ckeditor/ckeditor5-react'
 export const AddBlogs = () => {
   const [selectedimage, setSelectedImage] = useState([])
   const [avtarpreview, setAvatarpreview] = useState()
@@ -93,24 +93,25 @@ export const AddBlogs = () => {
               </div>
               </div>
               <label htmlFor=""><strong>Description</strong></label>
-              <Editor
-                onInit={(evt, editor) => editorRef.current = editor}
-                initialValue=""
-                init={{
-                  height: 400,
-                  menubar: false,
-                  plugins: [
-                    'advlist autolink lists link image charmap print preview anchor',
-                    'searchreplace visualblocks code fullscreen',
-                    'insertdatetime media table paste code help wordcount'
-                  ],
-                  toolbar: 'undo redo | formatselect | ' +
-                    'bold italic backcolor | alignleft aligncenter ' +
-                    'alignright alignjustify | bullist numlist outdent indent | ' +
-                    'removeformat | help',
-                  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-                }}
-              />          
+              <h2>Using CKEditor 5 from online builder in React</h2>
+              <CKEditor
+                    editor={ Editor }
+                    data="<p>Hello from CKEditor 5!</p>"
+                    onReady={ editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
                 <div className='btn_box'>
                 <button className='btn btn-primary my-2 give_margin' onClick={log}>Submit</button>
               </div>
